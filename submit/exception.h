973890b78c7d2094e556f5372503213fce2370b6
Yi Xie <loganxieyi@gmail.com>
2019-04-10 11:58:21 -0700

    upload again

---
+++ submit/exception.h
@@ -0,0 +1,12 @@
+//
+// Created by yi on 4/8/19.
+//
+
+#ifndef RECTANGLE_SEARCH_EXCEPTION_H
+#define RECTANGLE_SEARCH_EXCEPTION_H
+#include <stdlib.h>
+#include <iostream>
+//check the number of the arguments
+void CheckInputNum(int argc);
+void CannotOpenFile(std::string filename);
+#endif //RECTANGLE_SEARCH_EXCEPTION_H
---
+++ submit/rectangle.cc
@@ -0,0 +1,15 @@
+//
+// Created by yi on 4/8/19.
+//
+#include "rectangle.h"
+
+int Rectangle::GetArea() {
+    return abs((x2-x1)*(y2-y1));
+}
+
+bool Rectangle::operator<(Rectangle &rhs) {
+    if(this->GetArea() < rhs.GetArea()){
+        return true;
+    }
+    return false;
+}
